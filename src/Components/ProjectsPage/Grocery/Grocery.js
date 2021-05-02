46fc32ade313f5360e5ef6f59ad31caa0f804d77
Mohammad Eusha <eusha37@gmail.com>
2021-05-03 02:57:18 +0600

    color

--- src/Components/ProjectsPage/Grocery/Grocery.js
+++ src/Components/ProjectsPage/Grocery/Grocery.js
@@ -9,22 +9,22 @@ const Grocery = () => {
             <h1 style={{ color: "#f50057" }} className="text-center mb-5 pb-5">Grocery Store Details</h1>
             <Row >
                 <Col xs={12} md={6}>
-                    <h1>Grocery Store</h1>
+                    <h1 style={{ color: "#f50057" }}>Grocery Store</h1>
                     <p>The Website is about your daily grocery products.In this site you will at first find some cards about names of products,weight and prices.You will find a button in every card named as Buy Now.Click on the button then login with your Google account to order your products.</p>
-                    <h2>Features</h2>
+                    <h2 style={{ color: "#f50057" }}>Features</h2>
                     <ul>
                         <li> Google Sign-In</li>
                         <li>Admin Panel</li>
                         <li>Product Add System</li>
                         <li> Dashboard</li>
                     </ul>
-                    <h2>Technology</h2>
+                    <h2 style={{ color: "#f50057" }}>Technology</h2>
                     <ul>
                         <li>Frontend : HTML , CSS, Bootstrap, JavaScript, React JS</li>
                         <li> Backend : NodeJS, Express, Postman, Mongo DB.</li>
                         <li>Deploy Tools : Firebase, Heroku, Github.</li>
                     </ul>
-                    <h2>Frameworks</h2>
+                    <h2 style={{ color: "#f50057" }}>Frameworks</h2>
                     <ul>
                         <li>Bootstrap</li>
                     </ul>
